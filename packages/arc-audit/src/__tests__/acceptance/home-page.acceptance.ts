41d076883abb13927e285865fb5d69ab3b0921d6
Vishwas Yogi <vishwas.yogi@sourcefuse.com>
2023-08-28 14:22:38 +0530

    Initial Commit

---
+++ packages/arc-audit/src/__tests__/acceptance/home-page.acceptance.ts
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2022 Sourcefuse Technologies
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+import { Client } from "@loopback/testlab";
+import { STATUS_CODE } from "@sourceloop/core";
+import { AuditExampleApplication } from "../..";
+import { setupApplication } from "./test-helper";
+
+describe("HomePage", () => {
+  let app: AuditExampleApplication;
+  let client: Client;
+
+  before("setupApplication", async () => {
+    ({ app, client } = await setupApplication());
+  });
+
+  after(async () => {
+    await app.stop();
+  });
+
+  it("exposes a default home page", async () => {
+    await client
+      .get("/")
+      .expect(STATUS_CODE.OK)
+      .expect("Content-Type", /text\/html/);
+  });
+
+  it("exposes self-hosted explorer", async () => {
+    await client
+      .get("/explorer/")
+      .expect(STATUS_CODE.OK)
+      .expect("Content-Type", /text\/html/)
+      .expect(/<title>LoopBack API Explorer/);
+  });
+});
---
+++ packages/arc-audit/src/__tests__/lambda/audit.lambda.ts
@@ -0,0 +1,51 @@
+import { STATUS_CODE } from "@sourceloop/core";
+import dotenv from "dotenv";
+import * as jwt from "jsonwebtoken";
+import { describe, it } from "mocha";
+import request from "supertest";
+
+dotenv.config({
+  path: __dirname + "/./../../../.env",
+});
+const BASE_URL = process.env.LAMBDA_URL;
+
+const testUser = {
+  id: "5e6b1a4b-c946-96f5-7e26-9376e657dc0f",
+  username: "platform.admin@yopmail.com",
+  password: "test123!@#", //NOSONAR
+  permissions: [
+    "ViewAudit",
+    "CreateAudit",
+    "UpdateAudit",
+    "DeleteAudit",
+    "1",
+    "2",
+    "3",
+    "4",
+  ],
+};
+
+const token = jwt.sign(testUser, "test", {
+  expiresIn: 180000,
+  issuer: "sourcefuse",
+});
+
+describe("Audit App", () => {
+  it("should expose a self hosted server", async () => {
+    await request(BASE_URL)
+      .get("/explorer/")
+      .expect(STATUS_CODE.OK)
+      .expect("Content-Type", /text\/html/)
+      .expect(/<title>LoopBack API Explorer/);
+  });
+});
+
+describe("Audit Microservice", () => {
+  it("should return status 200 while fetching audit logs and token is passed", async () => {
+    const reqData = {};
+    await request(BASE_URL)
+      .get("/audit-logs")
+      .set("authorization", `Bearer ${token}`)
+      .expect(STATUS_CODE.OK);
+  });
+});
