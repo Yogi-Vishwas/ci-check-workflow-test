da96a4678e647e56c01009264aa14c559e8ff99e
Vishwas Yogi <vishwas.yogi@sourcefuse.com>
2023-08-29 18:11:04 +0530

    nth comment

--- packages/arc-audit/src/enums/permissions.enum.ts
+++ packages/arc-audit/src/enums/permissions.enum.ts
@@ -3,7 +3,7 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 export declare const enum PermissionKey {
-  CreateTodo = "CreateTodo",
-  UpdateTodo = "UpdateTodo",
-  DeleteTodo = "DeleteTodo",
+  CreateTodo = 'CreateTodo',
+  UpdateTodo = 'UpdateTodo',
+  DeleteTodo = 'DeleteTodo',
 }
